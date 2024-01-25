134b4006772ed7cc0fb1eb64b941fb1789f36cc1
LuminiferousGiraffe <andreas.quist99@gmail.com>
2024-01-25 18:55:36 -0500

    undid some deleting

--- src/main.jsx
+++ src/main.jsx
@@ -26,11 +26,6 @@ const router = createBrowserRouter([
         path: 'resume',
         element: <ResumePage />
       },
-      // },
-      // {
-      //   path: 'about',
-      //   element: <AboutPage />,
-      //},
       
     ],
   },
